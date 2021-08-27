bec2e9be1b27a520cf5abd9b6095384ad3416008
brenoc21 <brenocolaco3@gmail.com>
2021-08-27 01:42:33 -0300

    Challenge 4

--- TotalCross4.cs
+++ TotalCross4.cs
@@ -1,30 +1,42 @@
 using System;
 using System.Collections.Generic;
-
-public class Program
+class Program
 {
     public static int SimPropFrac(int maxDen)
     {
-			List<double> fractions = new List<double>();
-			int counter = 0;
-			int k = 1;
-			while(k <= maxDen){
-				for(int i = 1; i < k; i++){
-					fractions.Add(i/k);
-				}
-				k++;
-			}
-			for(int i = 0; i < fractions.Count; i++){
-				for(int j = 0; j < i; j++){
-					if(fractions[i] == fractions[j]){
-						break;
-					}else{
-						counter++;
-					}
-					
-				}
-				
-			}
-				return counter;
+        List<double> fractions = new List<double>();
+        int counter = 1;
+        int k = 1;
+        while (k <= maxDen)
+        {
+            for (int i = 1; i < k; i++)
+            {
+                fractions.Add((float)i / k);
+            }
+            k++;
+        }
+        for (int i = 0; i < fractions.Count; i++)
+        {
+
+            for (int j = 0; j < i; j++)
+            {
+
+                if (fractions[i] == fractions[j])
+                {
+                    break;
+                }
+                else
+                {
+                    if (j == i - 1)
+                    {
+                        counter++;
+                    }
+
+                }
+
+            }
+
+        }
+        return counter;
     }
 }
